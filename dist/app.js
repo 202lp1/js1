e8522cbb288f6d8bce772d30f1b98bf090ee01cc
a sullon m <asullom@gmail.com>
2020-09-25 11:32:05 -0500

    js

--- dist/app.js
+++ dist/app.js
@@ -2,10 +2,12 @@
 document.getElementById('formTask').addEventListener('submit', hola);
 
 function hola(e) {
-    let title = document.getElementsByName('title')[0].value;
+    let title = document.getElementById('title').value;
+    document.getElementById('title').value="";
+    let description = document.getElementById('description').value;
     console.log("title="+title);
-    document.getElementById("description").innerHTML = "Holaaaa ";
+    document.getElementById("description").innerHTML = description;
     e.preventDefault();
 }
 
-document.getElementById("description").innerHTML = "descripcion ejemplo ";
+//document.getElementById("description").innerHTML = "hi ";
